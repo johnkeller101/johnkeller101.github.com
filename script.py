28c078819178b6b85aba21e40c6e4aad8b2e006b
John Keller <john@Johns-MacBook-Pro.local>
2018-05-29 08:41:04 -0600

    tag test 4

--- script.py
+++ script.py
@@ -1,32 +1,8 @@
 # -*- coding: utf-8 -*-
 
-"""
-Little script that replaces comment characters to html,
-found in the *.hbs files.
-"""
-
 import os
 import glob
 
-if __name__ == '__main__':
-
-    os.chdir(os.path.join(os.environ['HOME'],
-                          "Downloads", "Casper-master"))
-    for filename in glob.glob("*.hbs"):
-        print(filename)
-
-        # Read in the file
-        with open(filename, 'r') as infile :
-          filedata = infile.read()
-
-        # Replace the target string
-        # Replacing comments
-        filedata = filedata.replace('{{!--', '<!--')
-        filedata = filedata.replace('--}}', '-->')
-
-        # Write the file out again
-        with open(filename, 'w') as outfile:
-          outfile.write(filedata)
 
 #!/usr/bin/env python
 
@@ -75,7 +51,7 @@ if not os.path.exists(tag_dir):
 for tag in total_tags:
     tag_filename = tag_dir + tag + '.md'
     f = open(tag_filename, 'a')
-    write_str = '---\nlayout: tagpage\ntitle: \"Tag: ' + tag + '\"\ntag: ' + tag + '\nrobots: noindex\n---\n'
+    write_str = '---\nlayout: tag\ntitle: \"Tag: ' + tag + '\"\ntag: ' + tag + '\nrobots: noindex\n---\n'
     f.write(write_str)
     f.close()
 print("Tags generated, count", total_tags.__len__())
---
+++ tag/rides.md
@@ -0,0 +1,6 @@
+---
+layout: tag
+title: "Tag: rides"
+tag: rides
+robots: noindex
+---
